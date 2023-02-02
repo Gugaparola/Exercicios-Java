4dabe0acdb8d8c5993875d004b476ae695c54404
Gugaparola <122889711+Gugaparola@users.noreply.github.com>
2023-02-01 21:55:19 -0300

    Add files via upload

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class Main  {
+    public static void main(String[] args) {
+        Scanner guga = new Scanner(System.in);
+        String gaga = guga.nextLine();
+        String []gagaa = gaga.split("");
+        String [] gigi = {"a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"};
+        for ( int a = 0; a< gagaa.length; a++){
+            System.out.println(gagaa[a]);
+            for(int b = 0; b< gigi.length;b++){
+
+            }
+        }
+    }
+}
